e21ff10a5a68b9d8828b851ef75de7377541e82a
Valerie Mettler <valeriemettler@gmail.com>
2015-09-22 16:36:16 -0700

    Add function to mix up puzzle pieces

--- js/main.js
+++ js/main.js
@@ -1,36 +1,58 @@
+//background position in correct order
+var bgpos = [[-1280, 0],
+             [-1700, 803],
+             [-2130, 803],
+             [-1280, -420],
+             [-1700, 373],
+             [-2130, 373]
+             ];
+//background position out of order
+var bgpos2 = [[-2130, 803],
+            [-2130, 373],
+            [-1280, -420],
+            [-1700, 803],
+            [-1700, 373],
+            [-1280, 0]
+            ];
+
+var x;
+var y;
+var z;
+//console.log(bgpos[0]); //[-1280, 0]
+//console.log(bgpos[0][0]); // -1280
+//console.log(bgpos[0][1]); // 0
+
+//console.log(bgpos[i]); //undefined
+//console.log(bgpos.length); // 6
+
+// var posStr = function(x, y){
+
+// };
+;
+
+var bkgPos = function (x,y) {
+    z = "" + x + "px " + y + "px";
+    //console.log(z);
+    return z;
+};
+
 $(document).ready(function() {
-        // var bgpos = ["-1280px 0px",
-        //              "-1700px 802.9px",
-        //              "-2130px 802.9px",
-        //              "-1280px -420px",
-        //              "-1700px 372.9px",
-        //              "-2130px 372.9px"];
 
-        var bgpos = [[-1280, 0],
-                     [-1700, 803],
-                     [-2130, 803],
-                     [-1280, -420],
-                     [-1700, 373],
-                     [-2130, 373]
-                     ];
 
+    var displayPic = function() {
         for (var i = 0; i < bgpos.length;i++) {
-            //console.log(i);
-            //console.log(bgpos[i]);
+                //a = bgpos[i][0];
+                //b = bgpos[i][1];
+                c = bgpos2[i][0];
+                d = bgpos2[i][1];
 
-            // $('.box' + i).css({
-            //     "background-position": bgpos[i],
-            // });
-
-            $('.box' + i).css({
-                "background-position": "" + bgpos[i][0] + "px " + bgpos[i][1] + "px",
-            });
-            console.log(bgpos[i]);
-            //console.log("" + bgpos2[i][0] + " " + bgpos2[i][1]);
-            console.log("" + bgpos[i][0] + "px " + bgpos[i][1] + "px");
+                //bkgPos(a, b);
+                 bkgPos(c, d);
+            $('.box' + i).css({"background-position": z});
+            //console.log("" + x + "px " + y + "px");
+            //console.log("" + bgpos[i][0] + "px " + bgpos[i][1] + "px");
+            //returns the bgpos list elements example: -1280px 0px
         }
-
-
-
 }
-);
+displayPic();
+});
