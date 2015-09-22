434420ee390390bf1be228b8ca7ffa36be63a921
Valerie Mettler <valeriemettler@gmail.com>
2015-09-22 12:33:19 -0700

    Broke down list of sprite positions in a new list

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
 $(document).ready(function() {
-        var bgpos = ["-1280px 0px", "-1700px 802.9px", "-2130px 802.9px", "-1280px -420px", "-1700px 372.9px", "-2130px 372.9px"];
+        // var bgpos = ["-1280px 0px",
+        //              "-1700px 802.9px",
+        //              "-2130px 802.9px",
+        //              "-1280px -420px",
+        //              "-1700px 372.9px",
+        //              "-2130px 372.9px"];
+
+        var bgpos = [[-1280, 0],
+                     [-1700, 803],
+                     [-2130, 803],
+                     [-1280, -420],
+                     [-1700, 373],
+                     [-2130, 373]
+                     ];
+
         for (var i = 0; i < bgpos.length;i++) {
             //console.log(i);
             //console.log(bgpos[i]);
 
+            // $('.box' + i).css({
+            //     "background-position": bgpos[i],
+            // });
+
             $('.box' + i).css({
-                "background-position": bgpos[i],
+                "background-position": "" + bgpos[i][0] + "px " + bgpos[i][1] + "px",
             });
+            console.log(bgpos[i]);
+            //console.log("" + bgpos2[i][0] + " " + bgpos2[i][1]);
+            console.log("" + bgpos[i][0] + "px " + bgpos[i][1] + "px");
         }
-// $('.box1').css( {
-//     "background-position":"-1280px 0px",
-// }
 
 
-// $('.box2').css( {
-//     "background-position":"-1700px 802.9px",
-// }
-// );
 
-// $('.box3').css( {
-//     "background-position":"-2130px 802.9px", //
-// }
-// );
-
-// $('.box4').css( {
-//     "background-position":"-1280px -420px", //
-// }
-// );
-
-// $('.box5').css( {
-//     "background-position":"-1700px 372.9px", //
-// }
-// );
-
-// $('.box6').css( {
-//     "background-position":"-2130px 372.9px", //
-// }
-// );
 }
 );
