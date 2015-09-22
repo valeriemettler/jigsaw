4e278cc763945f634d0d8a7875c380e6eb601f44
Valerie Mettler <valeriemettler@gmail.com>
2015-09-22 16:37:47 -0700

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -18,26 +18,16 @@ var bgpos2 = [[-2130, 803],
 var x;
 var y;
 var z;
-//console.log(bgpos[0]); //[-1280, 0]
-//console.log(bgpos[0][0]); // -1280
-//console.log(bgpos[0][1]); // 0
-
-//console.log(bgpos[i]); //undefined
-//console.log(bgpos.length); // 6
 
 // var posStr = function(x, y){
-
 // };
-;
 
 var bkgPos = function (x,y) {
     z = "" + x + "px " + y + "px";
-    //console.log(z);
     return z;
 };
 
 $(document).ready(function() {
-
 
     var displayPic = function() {
         for (var i = 0; i < bgpos.length;i++) {
@@ -49,9 +39,6 @@ $(document).ready(function() {
                 //bkgPos(a, b);
                  bkgPos(c, d);
             $('.box' + i).css({"background-position": z});
-            //console.log("" + x + "px " + y + "px");
-            //console.log("" + bgpos[i][0] + "px " + bgpos[i][1] + "px");
-            //returns the bgpos list elements example: -1280px 0px
         }
 }
 displayPic();
