6015751e0b30a5f297b174c8cad7c2e49ee34d89
Valerie Mettler <valeriemettler@gmail.com>
2015-09-23 00:26:37 -0700

    Add pure function that takes the length and width of any image and cuts it into 6 pieces.

--- js/main.js
+++ js/main.js
@@ -18,6 +18,17 @@ var bgpos2 = [[-2130, 803],
 var x;
 var y;
 
+var iPos = function(w, h){
+    var newbgPos = [[ -(w), h],
+                    [ -(w + (w/3)), h],
+                    [ -((w +(w/3)) + (w/3)), h],
+                    [ -(w),  h/2],
+                    [ -(w + (w/3)), h/2],
+                    [ -((w +(w/3)) + (w/3)), h/2],
+                   ];
+    console.log(newbgPos);
+    return newbgPos;
+};
 
 // var posStr = function(x, y){
 // };
@@ -26,14 +37,17 @@ var bkgPos = function (x,y) { return "" + x + "px " + y + "px"; };
 
 $(document).ready(function() {
 
+    //image length and width output to replace bgpos
+    var pic = iPos(1280, 800);
+
     var displayPic = function() {
-        for (var i = 0; i < bgpos.length;i++) {
-                //a = bgpos[i][0];
-                //b = bgpos[i][1];
-                c = bgpos2[i][0];
-                d = bgpos2[i][1];
+        for (var i = 0; i < pic.length;i++) {
+                a = pic[i][0];
+                b = pic[i][1];
+                // c = bgpos2[i][0];
+                // d = bgpos2[i][1];
                 //bkgPos(a, b);
-            $('.box' + i).css({"background-position": bkgPos(c, d)});
+            $('.box' + i).css({"background-position": bkgPos(a,b)});
         }
 }
 displayPic();
