16ff0ffef89ea02a9d44f8b2c5c607c8c2590919
Valerie Mettler <valeriemettler@gmail.com>
2015-09-24 12:28:13 -0700

    add shuffle function

--- js/main.js
+++ js/main.js
@@ -1,54 +1,47 @@
-//background position in correct order
-var bgpos = [[-1280, 0],
-             [-1700, 803],
-             [-2130, 803],
-             [-1280, -420],
-             [-1700, 373],
-             [-2130, 373]
-             ];
-//background position out of order
-var bgpos2 = [[-2130, 803],
-            [-2130, 373],
-            [-1280, -420],
-            [-1700, 803],
-            [-1700, 373],
-            [-1280, 0]
-            ];
+var shuffle = true;
 
-var iPos = function(){
+var iPos = function(shuffle) {
 
     var w = $('#pic').width();
-    console.log($('#pic').width());
-
     var h = $('#pic').height();
-    console.log($('#pic').height());
 
-    var newbgPos = [[ -(w), h],
-                    [ -(w + (w/3)), h],
-                    [ -((w +(w/3)) + (w/3)), h],
-                    [ -(w),  h/2],
-                    [ -(w + (w/3)), h/2],
-                    [ -((w +(w/3)) + (w/3)), h/2],
-                   ];
-    console.log(newbgPos);
+    var newbgPos = [
+                    [-(w), h],
+                    [-(w + (w / 3)), h],
+                    [-((w + (w / 3)) + (w / 3)), h],
+                    [-(w), h / 2],
+                    [-(w + (w / 3)), h / 2],
+                    [-((w + (w / 3)) + (w / 3)), h / 2],
+                ];
+
+    //http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    if (shuffle === true) {
+        for (var i = newbgPos.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = newbgPos[i];
+            newbgPos[i] = newbgPos[j];
+            newbgPos[j] = temp;
+        }
+    }
     return newbgPos;
 };
 
-var bkgPos = function (x,y) { return "" + x + "px " + y + "px"; };
+var bkgPos = function(x, y) {
+    return "" + x + "px " + y + "px";
+};
 
 $(document).ready(function() {
 
-    var pic = iPos();
+    var pic = iPos(shuffle);
 
     var displayPic = function() {
-        for (var i = 0; i < pic.length;i++) {
-                a = pic[i][0];
-                b = pic[i][1];
-                // c = bgpos2[i][0];
-                // d = bgpos2[i][1];
-                //bkgPos(a, b);
-            $('.box' + i).css({"background-position": bkgPos(a,b)});
+        for (var i = 0; i < pic.length; i++) {
+            a = pic[i][0];
+            b = pic[i][1];
+            $('.box' + i).css({
+                "background-position": bkgPos(a, b)
+            });
         }
-}
-displayPic();
+    }
+    displayPic();
 });
