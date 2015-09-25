a6cd41a67f700fb73c24b7efb2bf37613966100d
Valerie Mettler <valeriemettler@gmail.com>
2015-09-24 20:53:09 -0700

    add shuffle and solve puzzle button features

--- js/main.js
+++ js/main.js
@@ -1,47 +1,84 @@
 var shuffle = true;
+var newbgPos; //solved
+var newbgPoscopy; //shuffled
 
-var iPos = function(shuffle) {
+var iPos = function() {
 
     var w = $('#pic').width();
     var h = $('#pic').height();
 
-    var newbgPos = [
-                    [-(w), h],
-                    [-(w + (w / 3)), h],
-                    [-((w + (w / 3)) + (w / 3)), h],
-                    [-(w), h / 2],
-                    [-(w + (w / 3)), h / 2],
-                    [-((w + (w / 3)) + (w / 3)), h / 2],
-                ];
+    newbgPos = [
+        [-(w), h],
+        [-(w + (w / 3)), h],
+        [-((w + (w / 3)) + (w / 3)), h],
+        [-(w), h / 2],
+        [-(w + (w / 3)), h / 2],
+        [-((w + (w / 3)) + (w / 3)), h / 2],
+    ];
+    newbgPoscopy = newbgPos.slice();
 
-    //http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    if (shuffle === true) {
-        for (var i = newbgPos.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = newbgPos[i];
-            newbgPos[i] = newbgPos[j];
-            newbgPos[j] = temp;
-        }
-    }
-    return newbgPos;
+    return newbgPoscopy;
 };
 
 var bkgPos = function(x, y) {
     return "" + x + "px " + y + "px";
 };
 
-$(document).ready(function() {
+var setupclickShuf = function() {
+    $('#shuffle').on('click', function(event) {
+        event.stopPropagation();
+        shuffle = true;
+        var e = event.target;
+        shuf(e);
+    })
+};
 
-    var pic = iPos(shuffle);
+var displayPic = function() {
 
-    var displayPic = function() {
-        for (var i = 0; i < pic.length; i++) {
-            a = pic[i][0];
-            b = pic[i][1];
+    if (shuffle === false) {
+        for (var i = 0; i < newbgPos.length; i++) {
+            var a = newbgPos[i][0];
+            var b = newbgPos[i][1];
+            $('.box' + i).css({
+                "background-position": bkgPos(a, b)
+            });
+        }
+    } else {
+        for (var i = 0; i < newbgPoscopy.length; i++) {
+            var a = newbgPoscopy[i][0];
+            var b = newbgPoscopy[i][1];
             $('.box' + i).css({
                 "background-position": bkgPos(a, b)
             });
         }
     }
+};
+
+//shuffle: http://stackoverflow.com/a/12646864
+var shuf = function(el) {
+    for (var i = newbgPoscopy.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newbgPoscopy[i];
+        newbgPoscopy[i] = newbgPoscopy[j];
+        newbgPoscopy[j] = temp;
+    }
+    //console.log("copy", newbgPoscopy);
+    //console.log("original", newbgPos);
+    displayPic(newbgPoscopy);
+};
+
+var setupclickSolve = function() {
+    $('#solve').on('click', function(event) {
+        event.stopPropagation();
+        shuffle = false;
+        var e = event.target;
+        displayPic();
+    })
+};
+
+$(document).ready(function() {
+    iPos();
+    setupclickShuf();
+    setupclickSolve();
     displayPic();
 });
