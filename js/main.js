c9daeed229286c904df29f0f94e14e99d81aaf55
Valerie Mettler <valeriemettler@gmail.com>
2015-09-26 16:30:07 -0700

    Refactor untangling control flow

--- js/main.js
+++ js/main.js
@@ -1,84 +1,46 @@
-var shuffle = true;
-var newbgPos; //solved
-var newbgPoscopy; //shuffled
-
-var iPos = function() {
-
-    var w = $('#pic').width();
-    var h = $('#pic').height();
-
-    newbgPos = [
-        [-(w), h],
-        [-(w + (w / 3)), h],
-        [-((w + (w / 3)) + (w / 3)), h],
-        [-(w), h / 2],
-        [-(w + (w / 3)), h / 2],
-        [-((w + (w / 3)) + (w / 3)), h / 2],
-    ];
-    newbgPoscopy = newbgPos.slice();
-
-    return newbgPoscopy;
-};
-
-var bkgPos = function(x, y) {
-    return "" + x + "px " + y + "px";
-};
-
-var setupclickShuf = function() {
-    $('#shuffle').on('click', function(event) {
+var setUrlInputHandler = function() {
+    $("#buttonSeturl").on('click', function(event) {
         event.stopPropagation();
-        shuffle = true;
-        var e = event.target;
-        shuf(e);
-    })
-};
-
-var displayPic = function() {
-
-    if (shuffle === false) {
-        for (var i = 0; i < newbgPos.length; i++) {
-            var a = newbgPos[i][0];
-            var b = newbgPos[i][1];
-            $('.box' + i).css({
-                "background-position": bkgPos(a, b)
-            });
-        }
-    } else {
-        for (var i = 0; i < newbgPoscopy.length; i++) {
-            var a = newbgPoscopy[i][0];
-            var b = newbgPoscopy[i][1];
-            $('.box' + i).css({
-                "background-position": bkgPos(a, b)
-            });
-        }
-    }
-};
-
-//shuffle: http://stackoverflow.com/a/12646864
-var shuf = function(el) {
-    for (var i = newbgPoscopy.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = newbgPoscopy[i];
-        newbgPoscopy[i] = newbgPoscopy[j];
-        newbgPoscopy[j] = temp;
-    }
-    //console.log("copy", newbgPoscopy);
-    //console.log("original", newbgPos);
-    displayPic(newbgPoscopy);
-};
-
-var setupclickSolve = function() {
-    $('#solve').on('click', function(event) {
-        event.stopPropagation();
-        shuffle = false;
-        var e = event.target;
-        displayPic();
-    })
+        main();
+    });
 };
 
 $(document).ready(function() {
-    iPos();
-    setupclickShuf();
-    setupclickSolve();
-    displayPic();
+    setUrlInputHandler();
 });
+
+var main = function() {
+    var url = $('#url').val();
+    var dim = getPicSize(url);
+    var height = dim["height"];
+    var width = dim["width"];
+    var pieces = cutPic(width, height);
+    displayPieces(url, pieces);
+};
+
+var getPicSize = function(url) {
+    //send url to #pic img tag in html
+    //get width and height
+    //return width, height and url
+    console.log(url);
+    return {"width": "10px", "height": "5px"}
+};
+
+var cutPic = function(width, height){
+    console.log(width);
+    console.log(height);
+    var pieces = [
+        [1, 1],
+        [1, 1],
+        [1, 1],
+        [1, 1],
+        [1, 1],
+        [1, 1],
+    ];
+    return pieces;
+};
+
+var displayPieces = function(url, pieces) {
+    console.log(url);
+    console.log(pieces);
+};
