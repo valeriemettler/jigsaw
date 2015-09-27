08d20a7ede123a4f7ca5253c1f3f0a929454f697
Valerie Mettler <valeriemettler@gmail.com>
2015-09-26 18:50:54 -0700

    Add height and width of pieces and restyled pieces into top row and bottom row

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
     setUrlInputHandler();
 });
 
-var chop = function(w, h) {
-    $('.bigbox').css({
-        "width": w + 32,
-        "height": h - 20
-    });
+var chop = function(w, h, url) {
     var pieces = cutPic(w, h);
-    displayPieces(url, pieces);
+    var tall = h / 2;
+    var wide = w / 3;
+    console.log("xxx");
+    console.log(w,h,wide,tall);
+    displayPieces(url, pieces, tall, wide);
 }
 
 var getPicSize = function(url) {
@@ -28,27 +28,44 @@ var getPicSize = function(url) {
         var h = image.height;
         console.log(w);
         console.log(h);
-        chop(w, h);
+        chop(w, h, url);
     }
     image.src = url;
     return;
 };
 
-var cutPic = function(width, height) {
-    console.log(width);
-    console.log(height);
+var cutPic = function(w, h) {
+    console.log(w);
+    console.log(h);
     var pieces = [
-        [1, 1],
-        [1, 1],
-        [1, 1],
-        [1, 1],
-        [1, 1],
-        [1, 1],
+        [-(w), h],
+        [-(w + (w / 3)), h],
+        [-((w + (w / 3)) + (w / 3)), h],
+        [-(w), h / 2],
+        [-(w + (w / 3)), h / 2],
+        [-((w + (w / 3)) + (w / 3)), h / 2],
     ];
+    console.log(pieces);
     return pieces;
 };
 
-var displayPieces = function(url, pieces) {
-    console.log(url);
-    console.log(pieces);
+var bkgPos = function(x, y) {
+    return "" + x + "px " + y + "px";
+};
+
+var displayPieces = function(url, pieces, tall, wide) {
+    console.log("displayPieces url", url);
+    console.log("displayPieces pieces", pieces);
+    for (var i = 0; i < pieces.length; i++) {
+        var a = pieces[i][0];
+        var b = pieces[i][1];
+        console.log("a,b",a,b);
+        console.log(bkgPos(a, b));
+        $('.box' + i).css({
+            "background-position": bkgPos(a, b),
+            "height": tall + "px",
+            "width": wide + "px",
+            "background-image": "url('" + url + "')"
+        });
+    }
 };
